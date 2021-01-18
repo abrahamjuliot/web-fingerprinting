ec96abca9b22d41c7f41c2815fba6325af85de1a
Abraham <abrahamjuliot@gmail.com>
2021-01-18 11:55:42 -0800

    Update prototypeLies.js
    tidy

--- prototypeLies.js
+++ prototypeLies.js
@@ -1,28 +1,36 @@
 /* Prototype lies */
 
 const getIframe = () => {
-	try {
-		const numberOfIframes = window.length
-		const frag = new DocumentFragment()
-		const div = document.createElement('div')
-		frag.appendChild(div)
-		const ghost = () => `
+    try {
+        const numberOfIframes = window.length
+        const frag = new DocumentFragment()
+        const div = document.createElement('div')
+        frag.appendChild(div)
+        const ghost = () => `
 			height: 100vh;
 			width: 100vw;
 			position: absolute;
 			left:-10000px;
 			visibility: hidden;
 		`
-		div.innerHTML = `<div style="${ghost()}"><iframe></iframe></div>`
-		document.body.appendChild(frag)
-		const iframeWindow = window[numberOfIframes]
-		return { iframeWindow, div }
-	}
-	catch (error) {
-		return { iframeWindow: window, div: undefined }
-	}
+        div.innerHTML = `<div style="${ghost()}"><iframe></iframe></div>`
+        document.body.appendChild(frag)
+        const iframeWindow = window[numberOfIframes]
+        return {
+            iframeWindow,
+            div
+        }
+    } catch (error) {
+        return {
+            iframeWindow: window,
+            div: undefined
+        }
+    }
 }
-const { iframeWindow, div: iframeContainerDiv } = getIframe()
+const {
+    iframeWindow,
+    div: iframeContainerDiv
+} = getIframe()
 
 const getPrototypeLies = iframeWindow => {
     // Lie Tests
@@ -37,22 +45,22 @@ const getPrototypeLies = iframeWindow => {
         return hasInvalidValue ? true : false
     }
 
-	// calling the interface prototype on the function should throw a TypeError
+    // calling the interface prototype on the function should throw a TypeError
     const getCallInterfaceTypeErrorLie = (apiFunction, proto) => {
         try {
             new apiFunction()
-			apiFunction.call(proto)
+            apiFunction.call(proto)
             return true
         } catch (error) {
             return error.constructor.name != 'TypeError' ? true : false
         }
     }
 
-	// applying the interface prototype on the function should throw a TypeError
+    // applying the interface prototype on the function should throw a TypeError
     const getApplyInterfaceTypeErrorLie = (apiFunction, proto) => {
         try {
             new apiFunction()
-			apiFunction.apply(proto)
+            apiFunction.apply(proto)
             return true
         } catch (error) {
             return error.constructor.name != 'TypeError' ? true : false
@@ -106,25 +114,24 @@ const getPrototypeLies = iframeWindow => {
         'function () { [native code] }'
         `function () {\n    [native code]\n}`
         */
-		const apiFunctionToString = (
-			iframeWindow ? 
-			iframeWindow.Function.prototype.toString.call(apiFunction) :
-			apiFunction.toString()
-		)
-		const apiFunctionToStringToString = (
-			iframeWindow ? 
-			iframeWindow.Function.prototype.toString.call(apiFunction.toString) :
-			apiFunction.toString.toString()
-		)
-		const trust = name => ({
-			[`function ${name}() { [native code] }`]: true,
-			[`function get ${name}() { [native code] }`]: true,
-			[`function () { [native code] }`]: true,
-			[`function ${name}() {${'\n'}    [native code]${'\n'}}`]: true,
-			[`function get ${name}() {${'\n'}    [native code]${'\n'}}`]: true,
-			[`function () {${'\n'}    [native code]${'\n'}}`]: true
-		})
-		console.log(trust(name)[apiFunctionToString] || apiFunctionToString)
+        const apiFunctionToString = (
+            iframeWindow ?
+            iframeWindow.Function.prototype.toString.call(apiFunction) :
+            apiFunction.toString()
+        )
+        const apiFunctionToStringToString = (
+            iframeWindow ?
+            iframeWindow.Function.prototype.toString.call(apiFunction.toString) :
+            apiFunction.toString.toString()
+        )
+        const trust = name => ({
+            [`function ${name}() { [native code] }`]: true,
+            [`function get ${name}() { [native code] }`]: true,
+            [`function () { [native code] }`]: true,
+            [`function ${name}() {${'\n'}    [native code]${'\n'}}`]: true,
+            [`function get ${name}() {${'\n'}    [native code]${'\n'}}`]: true,
+            [`function () {${'\n'}    [native code]${'\n'}}`]: true
+        })
         return (
             !trust(name)[apiFunctionToString] ||
             !trust('toString')[apiFunctionToStringToString]
@@ -195,13 +202,13 @@ const getPrototypeLies = iframeWindow => {
         const lies = {
             // custom lie string names
             [`a: object constructor descriptor should return undefined properties`]: obj ? getUndefinedValueLie(obj, name) : false,
-			[`b: calling the interface prototype on the function should throw a TypeError`]: getCallInterfaceTypeErrorLie(apiFunction, proto),
-			[`c: applying the interface prototype on the function should throw a TypeError`]: getApplyInterfaceTypeErrorLie(apiFunction, proto),
+            [`b: calling the interface prototype on the function should throw a TypeError`]: getCallInterfaceTypeErrorLie(apiFunction, proto),
+            [`c: applying the interface prototype on the function should throw a TypeError`]: getApplyInterfaceTypeErrorLie(apiFunction, proto),
             [`d: creating a new instance of the function should throw a TypeError`]: getNewInstanceTypeErrorLie(apiFunction),
             [`e: extending the function on a fake class should throw a TypeError`]: getClassExtendsTypeErrorLie(apiFunction),
             [`f: setting prototype to null and converting to a string should throw a TypeError`]: getNullConversionTypeErrorLie(apiFunction),
             [`g: toString() and toString.toString() should return a native string in all frames`]: getToStringLie(apiFunction, name, iframeWindow),
-        	[`h: "prototype" in function should not exist`]: getPrototypeInFunctionLie(apiFunction),
+            [`h: "prototype" in function should not exist`]: getPrototypeInFunctionLie(apiFunction),
             [`i: "arguments", "caller", "prototype", "toString"  should not exist in descriptor`]: getDescriptorLie(apiFunction),
             [`j: "arguments", "caller", "prototype", "toString" should not exist as own property`]: getOwnPropertyLie(apiFunction),
             [`k: descriptor keys should only contain "name" and "length"`]: getDescriptorKeysLie(apiFunction),
@@ -218,21 +225,21 @@ const getPrototypeLies = iframeWindow => {
     // Lie Detector
     const createLieDetector = () => {
         const props = {} // lie list and detail
-		let propsSearched = [] // list of properties searched
+        let propsSearched = [] // list of properties searched
         return {
             getProps: () => props,
-			getPropsSearched: () => propsSearched,
+            getPropsSearched: () => propsSearched,
             searchLies: (obj, {
                 ignore
             } = {}) => Object.getOwnPropertyNames(!!obj && !!obj.prototype ? obj.prototype : !!obj ? obj : {}).forEach(name => {
                 if (name == 'constructor' || (ignore && new Set(ignore).has(name))) {
                     return
                 }
-				const objectNameString = /\s(.+)\]/
+                const objectNameString = /\s(.+)\]/
                 const apiName = `${
 					obj.name ? obj.name : objectNameString.test(obj) ? objectNameString.exec(obj)[1] : undefined
 				}.${name}`
-				propsSearched.push(apiName)
+                propsSearched.push(apiName)
                 try {
                     const proto = obj.prototype ? obj.prototype : obj
                     let res // response from getLies
@@ -269,68 +276,91 @@ const getPrototypeLies = iframeWindow => {
     } = lieDetector
 
     // search for lies: add properties to ignore if desired
-    
-	if ('AnalyserNode' in window) { searchLies(AnalyserNode) }
-    if ('AudioBuffer' in window) {  searchLies(AudioBuffer) }
-	if ('BiquadFilterNode' in window) { searchLies(BiquadFilterNode) }
-	searchLies(CanvasRenderingContext2D)
-	searchLies(Date)
-	searchLies(Intl.DateTimeFormat)
-	searchLies(Document)
-	searchLies(DOMRect)
-	searchLies(DOMRectReadOnly)
-	searchLies(Element)
-	searchLies(Function)
-	searchLies(HTMLCanvasElement)
-    searchLies(HTMLElement)
-	searchLies(HTMLIFrameElement)
-	if ('IntersectionObserverEntry' in window) { searchLies(IntersectionObserverEntry) }
-    searchLies(Math)
-	if ('MediaDevices' in window) { searchLies(MediaDevices) }
-    searchLies(Navigator)
-	searchLies(Node)
-	if ('OffscreenCanvasRenderingContext2D' in window) { searchLies(OffscreenCanvasRenderingContext2D) }
-	searchLies(Range)
-	searchLies(Intl.RelativeTimeFormat)
-    searchLies(Screen)
-	if ('SVGRect' in window) { searchLies(SVGRect) }
-	searchLies(TextMetrics)
-	if ('WebGLRenderingContext' in window) { searchLies(WebGLRenderingContext) }
-    if ('WebGL2RenderingContext' in window) { searchLies(WebGL2RenderingContext) }
 
-	/* potential targets:
-		RTCPeerConnection
-		Plugin
-		PluginArray
-		MimeType
-		MimeTypeArray
-		Worker
-		History
-	*/
+    if ('AnalyserNode' in window) {
+        searchLies(AnalyserNode)
+    }
+    if ('AudioBuffer' in window) {
+        searchLies(AudioBuffer)
+    }
+    if ('BiquadFilterNode' in window) {
+        searchLies(BiquadFilterNode)
+    }
+    searchLies(CanvasRenderingContext2D)
+    searchLies(Date)
+    searchLies(Intl.DateTimeFormat)
+    searchLies(Document)
+    searchLies(DOMRect)
+    searchLies(DOMRectReadOnly)
+    searchLies(Element)
+    searchLies(Function)
+    searchLies(HTMLCanvasElement)
+    searchLies(HTMLElement)
+    searchLies(HTMLIFrameElement)
+    if ('IntersectionObserverEntry' in window) {
+        searchLies(IntersectionObserverEntry)
+    }
+    searchLies(Math)
+    if ('MediaDevices' in window) {
+        searchLies(MediaDevices)
+    }
+    searchLies(Navigator)
+    searchLies(Node)
+    if ('OffscreenCanvasRenderingContext2D' in window) {
+        searchLies(OffscreenCanvasRenderingContext2D)
+    }
+    searchLies(Range)
+    searchLies(Intl.RelativeTimeFormat)
+    searchLies(Screen)
+    if ('SVGRect' in window) {
+        searchLies(SVGRect)
+    }
+    searchLies(TextMetrics)
+    if ('WebGLRenderingContext' in window) {
+        searchLies(WebGLRenderingContext)
+    }
+    if ('WebGL2RenderingContext' in window) {
+        searchLies(WebGL2RenderingContext)
+    }
+
+    /* potential targets:
+    	RTCPeerConnection
+    	Plugin
+    	PluginArray
+    	MimeType
+    	MimeTypeArray
+    	Worker
+    	History
+    */
 
     // return lies list and detail 
     const props = lieDetector.getProps()
-	const propsSearched = lieDetector.getPropsSearched()
+    const propsSearched = lieDetector.getPropsSearched()
     return {
         lieList: Object.keys(props).sort(),
         lieDetail: props,
-		lieCount: Object.keys(props).reduce((acc, key) => acc+props[key].length, 0),
-		propsSearched
+        lieCount: Object.keys(props).reduce((acc, key) => acc + props[key].length, 0),
+        propsSearched
     }
 }
 
 // start program
 const start = performance.now()
-const { lieList, lieDetail, lieCount, propsSearched } = getPrototypeLies(iframeWindow) // execute and destructure the list and detail
+const {
+    lieList,
+    lieDetail,
+    lieCount,
+    propsSearched
+} = getPrototypeLies(iframeWindow) // execute and destructure the list and detail
 if (iframeContainerDiv) {
-	iframeContainerDiv.parentNode.removeChild(iframeContainerDiv)
+    iframeContainerDiv.parentNode.removeChild(iframeContainerDiv)
 }
 const perf = performance.now() - start
 
 // check lies later in any function
-//lieList.includes('HTMLCanvasElement.toDataURL') // returns true or false
-//lieDetail['HTMLCanvasElement.toDataURL'] // returns the list of lies
+lieList.includes('HTMLCanvasElement.toDataURL') // returns true or false
+lieDetail['HTMLCanvasElement.toDataURL'] // returns the list of lies
 
-//console.log(propsSearched)
-//console.log(lieList)
-//console.log(lieDetail)
+console.log(propsSearched)
+console.log(lieList)
+console.log(lieDetail)
